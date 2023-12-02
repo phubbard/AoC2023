e98f6a3a392f827440c2c9f94628951a623d918a
Paul Hubbard <pfh@phfactor.net>
2023-12-02 13:33:21 -0800

    beginning day 2

--- p02.py
+++ p02.py
@@ -1,5 +1,11 @@
 from utils import get_data_as_lines
 
+sample_constraint = {'red': 12, 'green': 13, 'blue': 14}
+full_constraint = {'red': 12, 'green': 13, 'blue': 14}
+
+
+
+
 if __name__ == '__main__':
     sample = get_data_as_lines(2, 's')
     full = get_data_as_lines(2)
