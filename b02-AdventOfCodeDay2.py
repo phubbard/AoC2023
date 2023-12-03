93986d1f99959c91364a8f3e26729769f36f98a7
Brad Hyslop @ mimic <bhyslop@scaleinvariant.org>
2023-12-03 10:42:11 -0800

    Determined superset of symbols

--- b02-AdventOfCodeDay2.py
+++
@@ -1,9 +0,0 @@
-
-"""
-Attempt to figure out what copilot is drawing from concerning the
-current Advent of Code challenge.  This is the second day of the
-challenge.
-"""
-
-colors = ('red', 'green', 'blue')
-bag_contents = {'red': 12}
--- b03.py
+++ b03.py
@@ -1,6 +1,26 @@
 from utils import log, load_2d_arrays
 
+# Make a set of known symbols
+KNOWN_SYMBOLS = set("+*#$/%&@!?-=<>")
 
 if __name__ == '__main__':
-    sample, full = load_2d_arrays(3)
-    log.info(f"{sample=}")
+    sample_data, full_data = load_2d_arrays(3)
+    for tag, dataset, expected_answer in [
+        ("sample", sample_data, 7),
+        ("full", full_data, 336),
+    ]:
+        for row in dataset:
+            for character in row:
+                if character == '.': continue
+                is_integer = False
+                # First, is character an integer?
+                try:
+                    int(character)
+                    is_integer = True
+                except ValueError:
+                    pass
+                if is_integer: continue
+
+                if character not in KNOWN_SYMBOLS:
+                    raise ValueError(f"Unknown symbol: {character}")
+        log.info(f"{tag=} {expected_answer=}")
