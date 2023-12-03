a95a045efbe0a798bd12afcc3e208f2da4825ba5
Brad Hyslop @ mimic <bhyslop@scaleinvariant.org>
2023-12-03 11:41:29 -0800

    Whoops, forgot to skip known numbers.

--- b03.py
+++ b03.py
@@ -3,12 +3,14 @@ from utils import log, load_2d_arrays, safe_insert
 # Make a set of known symbols
 KNOWN_SYMBOLS = set("+*#$/%&@!?-=<>")
 
+DIGITS = set("0123456789")
 
 class Grid:
     def __init__(self, array_of_arrays):
         self.GRID_MAX_X = len(array_of_arrays[0])
         self.GRID_MAX_Y = len(array_of_arrays)
 
+        # Construct Cells
         self.GRID_CELLS = {}
         for y in range(self.GRID_MAX_Y):
             previous_cell = None
@@ -20,6 +22,7 @@ class Grid:
                 if previous_cell: previous_cell.cell_set_right(cell)
                 previous_cell = cell
 
+        # Associate Cell neighbors
         for x in range(self.GRID_MAX_X):
             for y in range(self.GRID_MAX_Y):
                 cell = self.GRID_CELLS[(x, y)]
@@ -31,6 +34,13 @@ class Grid:
                 self.__grid_try_add_neighbor(cell, (x-1, y+1))
                 self.__grid_try_add_neighbor(cell, (x-0, y+1))
                 self.__grid_try_add_neighbor(cell, (x+1, y+1))
+
+        # Build Numbers
+        numbers = set()
+        for cell in self.GRID_CELLS.values():
+            if not cell.CELL_CHAR in DIGITS: continue
+            numbers.add(Number(cell))
+        self.GRID_NUMBERS = frozenset(numbers)
 
     def __grid_try_add_neighbor(self, cell, ordinate):
         if ordinate in self.GRID_CELLS:
@@ -49,6 +59,9 @@ class Cell:
     def cell_set_right(self, cell):
         self.__cell_right = cell
 
+    def cell_get_right(self):
+        return self.__cell_right
+
     def cell_add_neighbor(self, cell):
         self.__cell_neighbors[cell] = True
 
@@ -57,6 +70,22 @@ class Cell:
         neighbor_string = "".join(neighbor_chars)
         right_coda = f" right:{self.__cell_right.CELL_CHAR}" if self.__cell_right else ""
         return f"Cell '{self.CELL_CHAR}' at ({self.CELL_X}, {self.CELL_Y}) neighbors:{neighbor_string}{right_coda}"
+
+class Number:
+    def __init__(self, first_cell):
+        included_cells = set()
+        if first_cell.CELL_CHAR not in DIGITS:
+            raise Exception(f"Cannot create a Number from a non-digit cell: {first_cell}")
+        value = 0
+        current_cell = first_cell
+        while current_cell and current_cell.CELL_CHAR in DIGITS:
+            included_cells.add(current_cell)
+            value = value * 10 + int(current_cell.CELL_CHAR)
+            current_cell = current_cell.cell_get_right()
+        # finally, store as immutable set
+        self.NUMBER_VALUE = value
+        self.NUMBER_INCLUDED_CELLS = frozenset(included_cells)
+
 
 if __name__ == '__main__':
     sample_data, full_data = load_2d_arrays(3)
@@ -68,4 +97,11 @@ if __name__ == '__main__':
         grid = Grid(dataset)
         for cell in grid.GRID_CELLS.values():
             log.info(cell)
+        for number in grid.GRID_NUMBERS:
+            log.info(f'{number.NUMBER_VALUE=}')
         raise Exception("TODO: Implement me!")
+
+
+## Todo cleanups, that explore vscode features:
+##  - [ ] remove grid member of cell
+##  - [ ] alter setting of self.GRID_CELLS to assign once and use set
