6afe55230ed114d6bebb4fc044b1cf6dd7308098
Paul Hubbard <pfh@phfactor.net>
2023-12-03 12:56:44 -0800

    part 1 solved by myself and brad.

--- p03.py
+++ p03.py
@@ -63,9 +63,9 @@ def get_neighbors(row, col, data):
 
 def valid_neighbors(row, col, data):
     adjacent = get_neighbors(row, col, data)
-    valid = True
+    valid = False
     if any([x1 in KNOWN_SYMBOLS for x1 in adjacent]):
-        valid = False
+        valid = True
         log.debug(f"found symbol adjacent at {row=} {col=}")
     return valid
 
@@ -74,17 +74,17 @@ def coalesce_and_score(row, col, data) -> tuple:
     # For a numeric cell, coalesce righthand digits while checking for
     # neighboring cells that are symbols. Returns the row, col, and score
     # pointing to the cell after the last digit.
-    valid = True
+    valid = False
     digits = 1
     string = data[row][col]
-    if not valid_neighbors(row, col, data):
-        valid = False
+    if valid_neighbors(row, col, data):
+        valid = True
 
     right_char = right(row, col, data)
     while right_char and right_char.isdigit():
         col += 1
-        if not valid_neighbors(row, col, data):
-            valid = False
+        if valid_neighbors(row, col, data):
+            valid = True
         string += right_char
         digits += 1
         right_char = right(row, col, data)
@@ -119,3 +119,4 @@ if __name__ == '__main__':
     sample, full = load_2d_arrays(3)
     log.info(f"{sample=}")
     log.info(f"{process_data(sample)=}")
+    log.info(f"{process_data(full)=}")
