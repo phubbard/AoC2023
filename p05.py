a9f291adb9b596b73198038577013ef0dcdf7a6d
Paul Hubbard <pfh@phfactor.net>
2023-12-08 17:08:54 -0800

    part 1 done

--- p05.py
+++ p05.py
@@ -1,7 +1,9 @@
+from utils import get_data_lines
+
 
 class Transform:
-    def __init__(self):
-        self.name = 'Unknown'
+    def __init__(self, comment: str):
+        self.name = comment.split('-to-')[1].split()[0]
         self.src_ranges = []
         self.dest_ranges = []
 
@@ -13,25 +15,62 @@ class Transform:
         for idx, (start, count) in enumerate(self.src_ranges):
             if src >= start and src < start + count:
                 return self.dest_ranges[idx][0] + (src - start)
-        raise ValueError(f"Could not find {src} in {self.src_ranges}")
 
-def generate_map(input_lines: list, name: str) -> dict:
+        return src
 
-    maps = []
+
+def generate_xform(input_lines: list, comment: str) -> Transform:
+
+    rc = Transform()
+    rc.name = name
+
     for line in input_lines:
         numbers = [int(x) for x in line.split(' ')]
         assert len(numbers) == 3
+        rc.add_range(numbers[0], numbers[1], numbers[2])
+    return rc
 
-        instance = Transform(numbers[1], numbers[0], numbers[2], name)
-        maps.append(instance)
 
-    return maps
+def parse_inp_lines(data: list):
+    xforms = []
+    current_xformer = None
+    for line in data:
+        if len(line.strip()) == 0:
+            continue
+        if 'seeds' in line:
+            seeds = [int(x) for x in line.split(':')[1].split()]
+        elif 'map' in line:
+            current_xformer = Transform(line)
+            xforms.append(current_xformer)
+        else:
+            numbers = [int(x) for x in line.split(' ')]
+            assert len(numbers) == 3
+            current_xformer.add_range(numbers[0], numbers[1], numbers[2])
+
+    min_final = None
+    for seed in seeds:
+        print(f"Seed {seed}")
+        for xformer in xforms:
+            seed = xformer.lookup(seed)
+            print(f"  {xformer.name} {seed}")
+        if min_final is None or seed < min_final:
+            min_final = seed
+        print(f"{min_final=}")
+
+    print(f"{min_final=}")
 
 
 if __name__ == '__main__':
-    tdata = '''50 98 2
-52 50 48'''
-    tname = 'seed-to-soil'
-    tlines = tdata.split('\n')
-    tmaps = generate_map(tlines, tname)
-    print(tmaps)
+    sample, final = get_data_lines(5)
+    parse_inp_lines(sample)
+    parse_inp_lines(final)
+
+
+#     tdata = '''50 98 2
+# 52 50 48'''
+#     tname = 'seed-to-soil'
+#     tlines = tdata.split('\n')
+#     tmap = generate_xform(tlines, tname)
+#     assert tmap.lookup(50) == 52
+#     assert tmap.lookup(1) == 1
+#     assert tmap.lookup(99) == 51
