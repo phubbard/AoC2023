637360ed46e3f44c4a2452acc4a42ef5fe0b6422
Brad Hyslop @ mimic <bhyslop@scaleinvariant.org>
2023-12-08 20:13:57 -0800

    Better form, but still bugs evident in chatter.

--- b05x.py
+++ b05x.py
@@ -10,6 +10,10 @@ class Transformer:
 
     def add_range(self, destination_start, source_start, count):
         self.__range_map.append((destination_start, source_start, count))
+        
+    def range_descriptions(self):
+        return [f"{source_start=} goes to {destination_start=} for {count}" 
+                for destination_start, source_start, count in self.__range_map]  
 
     def transform(self, source):
         for destination_start, source_start, count in self.__range_map:
@@ -17,49 +21,60 @@ class Transformer:
                 return destination_start + (source - source_start)
         return source
 
-    def refract(self, *monoranges):
-        """Given a set of monoranges, return new monoranges after passing through
-        the transformer, including the direct pass through logic."""
-        rv = []
-        for monorange in monoranges:
-            for destination_start, source_start, count in self.__range_map:
-                if monorange.MR_START < source_start:
-                    # This monorange is entirely before the start of the range
-                    # of this transformer.  Just pass it through.
-                    rv.append(monorange)
-                elif monorange.MR_START < source_start + count:
-                    # This monorange starts within the range of this transformer.
-                    # It may end within the range of this transformer, or it may
-                    # extend beyond the range of this transformer.
-                    if monorange.MR_TERMINAL <= source_start + count:
-                        # This monorange ends within the range of this transformer.
-                        # Pass it through.
-                        rv.append(monorange)
-                    else:
-                        # This monorange extends beyond the range of this transformer.
-                        # Split it into two monoranges, one that ends at the end of
-                        # the range of this transformer, and one that starts at the
-                        # end of the range of this transformer.
-                        rv.append(Monorange(monorange.MR_START, source_start + count - monorange.MR_START))
-                        rv.append(Monorange(source_start + count, monorange.MR_TERMINAL - (source_start + count)))
+    def refract(self, monorange):
+        """Given a monorange, return a set of monoranges after applying
+        the transformer.  Additionally return monoranges for any original
+        values not transformed."""
+        uncut_monoranges = [monorange]
+
+        cut_monoranges = []
+        for destination_start, source_start, count in self.__range_map:
+            for monorange in uncut_monoranges:
+                if monorange.MR_LAST < source_start:
+                    log.info("Whole monorange is before the rangemap segment: no transform")
+                    cut_monoranges.append(monorange)
+                elif monorange.MR_FIRST >= source_start + count:
+                    log.info("Whole monorange is after the rangemap segment: no transform")
+                    cut_monoranges.append(monorange)
                 else:
-                    # This monorange is entirely after the end of the range of this
-                    # transformer.  Just pass it through.
-                    rv.append(monorange)
-        return rv
+                    log.info("At least one portion of the monorange must be transformed...")
+                    prefix  = None
+                    infix   = None
+                    postfix = None
+
+                    first = monorange.MR_FIRST
+                    last  = monorange.MR_LAST
+                    if first < source_start:
+                        log.info("The first part of the monorange is not transformed")
+                        prefix = Monorange(first, source_start-1)
+                        first = source_start
+                    if last >= source_start + count:
+                        log.info("The last part of the monorange is not transformed")
+                        postfix = Monorange(source_start+count, last)
+                        last = source_start + count - 1
+                    log.info("The middle part of the monorange is transformed")
+                    infix = Monorange(destination_start + (first - source_start),
+                                      destination_start + (last  - source_start))
+                    if prefix:  cut_monoranges.append(prefix)
+                    if infix:   cut_monoranges.append(infix)
+                    if postfix: cut_monoranges.append(postfix)
+            uncut_monoranges = cut_monoranges
+            cut_monoranges = []
+        log.info(f"Returning   --> {uncut_monoranges}")
+        return uncut_monoranges
+
 
 class Monorange:
 
-    def __init__(self, start, count):
-        self.MR_START    = start
-        self.MR_COUNT    = count
-        self.MR_TERMINAL = start + count
+    def __init__(self, first, last):
+        self.MR_FIRST = first
+        self.MR_LAST  = last
 
     def __repr__(self):
-        return f"Monorange({self.MR_START}, {self.MR_COUNT})"
+        return f"Monorange({self.MR_FIRST}-{self.MR_LAST})"
     
     def __str__(self):
-        return f"Monorange({self.MR_START}, {self.MR_COUNT})"
+        return f"Monorange({self.MR_FIRST}-{self.MR_LAST})"
 
 
 if __name__ == '__main__':
@@ -117,13 +132,21 @@ if __name__ == '__main__':
         for x in range(0, len(seeds) // 2):
             seed  = seeds[x*2]
             count = seeds[x*2+1]
-            monoranges.append(Monorange(seed, count))
+            monoranges.append(Monorange(seed, seed + count - 1))
         for transformer in transformers:
-            log.info(f"Sending {len(monoranges)} through transformer {transformer.TRANSFORMER_TAG}...")
-            monoranges = transformer.refract(*monoranges)
+            log.info(f"Current monoranges are...")
+            for monorange in monoranges:
+                log.info(f"    {monorange}")
+            log.info(f"Sending {len(monoranges)} through transformer {transformer.TRANSFORMER_TAG} with")
+            for description in transformer.range_descriptions():
+                log.info(f"    {description}")
+            new_monoranges = []
+            for monorange in monoranges:
+                new_monoranges += transformer.refract(monorange)
+            monoranges = new_monoranges
         log.info(f"finally {len(monoranges)=}")
 
-        found_p2_answer = min(monorange.MR_START for monorange in monoranges)
+        found_p2_answer = min(monorange.MR_FIRST for monorange in monoranges)
         log.info(f"{expected_p2_answer=} {found_p2_answer=}")
         assert found_p2_answer == expected_p2_answer
 
