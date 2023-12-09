6fde5000a13c6d0ba86d2b5829013b17e4977cfe
Brad Hyslop @ mimic <bhyslop@scaleinvariant.org>
2023-12-09 09:44:58 -0800

    Zipping works.

--- b06.py
+++ b06.py
@@ -6,8 +6,24 @@ if __name__ == '__main__':
     sample_data = ["Time:      7  15   30",
                    "Distance:  9  40  200"]
     for dataset, expected_p1_answer, expected_p2_answer in [
-                (sample_data,        -1,      -1),
-            ]:
+                    (sample_data,        -1,      -1),
+                ]:
+        time_array = None
+        distance_array = None
+        for row in dataset:
+            if 'Time' in row:
+                if time_array is not None: raise Exception("Multiple time arrays")
+                time_array = [int(x) for x in dataset[0].split(':')[1].split()]
+            elif 'Distance' in row:
+                if distance_array is not None: raise Exception("Multiple distance arrays")
+                distance_array = [int(x) for x in dataset[1].split(':')[1].split()]
+            else: raise Exception(f"Unknown row: {row}")
+        assert len(time_array) == len(distance_array), \
+            "Time and distance arrays must be the same length"
+        
+        # Now we have two integer arrays to process
+        for time, distance in zip(time_array, distance_array):
+            log.info(f"Considering {time=} {distance=} ...")
         
         found_p1_answer = 0
         log.info(f"{expected_p1_answer=} {found_p1_answer=}")
