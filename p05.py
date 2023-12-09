9e0749505ba312411774d13154c22ee60c7d8434
Paul Hubbard <pfh@phfactor.net>
2023-12-08 20:05:19 -0800

    starting collab with chris

--- p05.py
+++ p05.py
@@ -1,4 +1,11 @@
 from utils import get_data_lines
+from multiprocessing import Pool
+from functools import lru_cache
+from math import pow
+from itertools import batched, starmap
+
+INT_MAX = pow(2, 35) - 1
+xforms = []
 
 
 class Transform:
@@ -19,16 +26,34 @@ class Transform:
         return src
 
 
+@lru_cache(maxsize=1048596)
+def run_xforms(value: int) -> int:
+    global xforms
+    global_min = INT_MAX
+    for xformer in xforms:
+        value = xformer.lookup(value)
+        global_min = min(global_min, value)
+    return global_min
+
+
+def worker_fn(seed_min, seed_max) -> int:
+    # Process a range of seeds and return the minimum value found
+    global_min = run_xforms(seed_min)
+    for seed in range(seed_min, seed_max):
+        global_min = int(min(global_min, run_xforms(seed)))
+    print(f"Worker {seed_min},{seed_max} {seed_max - seed_min=} returning {global_min=}")
+    return global_min
+
+
 def parse_inp_lines(data: list):
-    xforms = []
+    global xforms
     current_xformer = None
-    seed_ranges = []
     for line in data:
         if len(line.strip()) == 0:
             continue
         if 'seeds' in line:
             seeds = [int(x) for x in line.split(':')[1].split()]
-            
+
         elif 'map' in line:
             current_xformer = Transform(line)
             xforms.append(current_xformer)
@@ -37,17 +62,32 @@ def parse_inp_lines(data: list):
             assert len(numbers) == 3
             current_xformer.add_range(numbers[0], numbers[1], numbers[2])
 
+    # part 1
     min_final = None
     for seed in seeds:
-        print(f"Seed {seed}")
+        # print(f"Seed {seed}")
         for xformer in xforms:
             seed = xformer.lookup(seed)
-            print(f"  {xformer.name} {seed}")
+            # print(f" - {xformer.name} {seed}")
         if min_final is None or seed < min_final:
             min_final = seed
-        print(f"{min_final=}")
+        # print(f"{min_final=}")
 
-    print(f"{min_final=}")
+    print(f"part 1 {min_final=}")
+
+    ########################
+    print("part 2")
+    seed_ranges = list(batched(seeds, 2))
+
+    total_search_count = sum([x[0] + x[1] for x in seed_ranges])
+    print(f"{total_search_count=}")
+    pool = Pool()
+
+    l_global_min = INT_MAX
+    results = pool.starmap(worker_fn, seed_ranges)
+    l_global_min = int(min(results))
+
+    print(f"part 2 {l_global_min=}")
 
 
 if __name__ == '__main__':
