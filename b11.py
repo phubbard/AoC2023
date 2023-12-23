f3eb6f1a1146b355d001140309e90fbdf97f56dd
Brad Hyslop <bhyslop@scaleinvariant.org>
2023-12-22 20:34:04 -0800

    space expansion seems healthy.

--- b11.py
+++ b11.py
@@ -1,27 +1,63 @@
 from utils import get_data_lines, log
 
-DOESNT_WORK_BRAD_GOT_THERE_LATE
 
-sample_data = \
-"""0 3 6 9 12 15
-1 3 6 10 15 21
-10 13 16 21 30 45
-"""
+class Galaxy:
+    def __init__(self, row, column):
+        self.GALAXY_ROW    = row
+        self.GALAXY_COLUMN = column
 
-def predict_next_number(numbers):
-    first_number_list = numbers[:-1]
-    second_number_list = numbers[1:]
-    delta_list = [s - f for f, s in zip(first_number_list, second_number_list)]
+
+class Space:
+    def __init__(self):
+        self.__space_galaxies   = []
+        self.__space_rows       = {} # row number -> list of galaxies
+        self.__space_columns    = {} # column number -> list of galaxies
+        self.__space_max_column = 0
+        self.__space_max_row    = 0
+
+    def add_space(self, row, column, isGalaxy):
+        self.__space_max_column = max(self.__space_max_column, column)
+        self.__space_max_row    = max(self.__space_max_row,    row)
+
+        coda = []
+        if isGalaxy:
+            galaxy = Galaxy(row, column)
+            self.__space_galaxies.append(galaxy)
+            coda.append(galaxy)
+        self.__space_rows[row]       = self.__space_rows.get(row, [])       + coda
+        self.__space_columns[column] = self.__space_columns.get(column, []) + coda
+
+    def grow_space(self):
+        delta = 0.5
+
+        for column in range(self.__space_max_column + 1):
+            if len(self.__space_columns[column]) > 0: continue
+            self.__space_columns[column + delta] = []
+        self.__space_columns = {c: self.__space_columns[c] for c in sorted(self.__space_columns.keys())}
+        
+        for row in range(self.__space_max_row + 1):
+            if len(self.__space_rows[row]) > 0: continue
+            self.__space_rows[row + delta] = []
+        self.__space_rows = {r: self.__space_rows[r] for r in sorted(self.__space_rows.keys())}
+
+        log.info(f"Now cols are {self.__space_columns.keys()}")
+        log.info(f"Now rows are {self.__space_rows.keys()}")
 
 
 if __name__ == '__main__':
-    sample_data, full_data = get_data_lines(9)
+    sample_data, full_data = get_data_lines(11)
     for tag, dataset, expected_p1_answer, expected_p2_answer in [
-                ("sample", sample_data,       -1,       -1),
+                ("sample", sample_data,       374,       -1),
             ]:
         
+        space = Space()
+
         log.info(f"Considering {tag=}")
         if expected_p1_answer > 0:
+            for row, line in enumerate(dataset):
+                for column, char in enumerate(line):
+                    space.add_space(row, column, char == '#')
+            space.grow_space()
             found_p1_answer = 3333
             log.info(f"{found_p1_answer=} with {expected_p1_answer=}")
             assert found_p1_answer == expected_p1_answer
