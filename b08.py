1289541b39f07f39e701e8b9e2a7d1cd6249d187
Brad Hyslop <bhyslop@scaleinvariant.org>
2023-12-22 16:12:04 -0800

    Possible successful capture of iinputs.

--- b08.py
+++ b08.py
@@ -1,5 +1,33 @@
 from utils import get_data_lines, log
 
+
+class Network:
+    def __init__(self):
+        self.__nodes = {}
+
+    def add_node(self, assignment_string):
+        tag_me, directions = assignment_string.split(' = ')
+        tag_left, tag_right = directions.replace('(', '').replace(')', '').split(', ')
+        node = Node(tag_me, tag_left, tag_right)
+        self.__nodes[tag_me] = node
+
+    def crosslink(self):
+        for node in self.__nodes.values():
+            left  = self.__nodes[node.NODE_TAG_LEFT]
+            right = self.__nodes[node.NODE_TAG_RIGHT]
+            node.crosslink_nodes(left, right)
+
+
+class Node:
+    def __init__(self, tag_me, tag_left, tag_right):
+        self.NODE_TAG_me    = tag_me
+        self.NODE_TAG_LEFT  = tag_left
+        self.NODE_TAG_RIGHT = tag_right
+
+    def crosslink_nodes(self, left, right):
+        self.NODE_LEFT  = left
+        self.NODE_RIGHT = right
+
 if __name__ == '__main__':
     sample_data, full_data = get_data_lines(8)
     for tag, dataset, expected_p1_answer, expected_p2_answer in [
@@ -7,4 +35,13 @@ if __name__ == '__main__':
                 ("full",     full_data,      -1,      -1),
             ]:
         log.info(f"Considering {tag=}")
+        network = Network()
+        directions = None
+        for line in dataset:
+            if directions is None:
+                directions = line
+            elif len(line) > 0:
+                network.add_node(line)
+        network.crosslink()
+            
     log.info(f"Success")
