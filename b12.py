8af9b5b6fda384a339132041aabe0e8666c87425
Brad Hyslop <bhyslop@scaleinvariant.org>
2023-12-24 14:17:22 -0800

    Candidate answer for p2 day 12.

--- b12.py
+++ b12.py
@@ -112,9 +112,9 @@ if __name__ == '__main__':
     sample_data, full_data = get_data_lines('12')
 
     for tag, dataset, expected_p1_answer, expected_p2_answer in [
-                ("sample", sample_data,       -1,      -1),
-                ("raw_s2", raw_12s2_data,     21,  525152),
-                # ("full",   full_data,       8180,      -1),
+                ("sample", sample_data,       -1,               -1),
+                ("raw_s2", raw_12s2_data,     21,           525152),
+                ("full",   full_data,       8180,  620189727003627),
             ]:
 
         if expected_p1_answer > -1:
