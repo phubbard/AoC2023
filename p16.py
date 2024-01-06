6ddb266a09320401d02817cdd5f32a64e6650b9d
Paul Hubbard <pfh@phfactor.net>
2024-01-06 12:19:16 -0800

    starting p16

---
+++ p16.py
@@ -0,0 +1,12 @@
+from utils import load_2d_arrays, log
+
+
+work_queue = []
+
+def process_step(map: list, row: int, col: int, direction: str):
+    pass
+
+
+if __name__ == '__main__':
+    sample, full = load_2d_arrays(16)
+    print(sample)
