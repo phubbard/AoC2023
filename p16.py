91a6c326738e96a37edd9c0603f36210512326a9
Paul Hubbard <pfh@phfactor.net>
2024-01-06 16:48:40 -0800

    p2 done - fun problem.

--- p16.py
+++ p16.py
@@ -2,7 +2,6 @@ from utils import load_2d_arrays, log, make_2d_array
 
 
 work_queue = []
-energized_map = [[]]
 
 
 def next_row_col(row, col, direction):
@@ -13,16 +12,15 @@ def next_row_col(row, col, direction):
     return nrc[direction](row, col)
 
 
-def process_step(map: list, row: int, col: int, direction: str):
+def process_step(map: list, en_map: list, row: int, col: int, direction: str):
     mirror_backslash = {'R': 'D', 'D': 'R', 'L': 'U', 'U': 'L'}
     mirror_slash = {'R': 'U', 'D': 'L', 'L': 'D', 'U': 'R'}
 
     if row < 0 or col < 0 or row >= len(map) or col >= len(map[0]):
-        log.debug(f"Out of bounds: {row}, {col}")
         return
 
     cell = map[row][col]
-    energized_map[row][col] = 1
+    en_map[row][col] = 1
     match cell:
         case '.':
             # Empty space - no-op
@@ -60,16 +58,17 @@ def process_step(map: list, row: int, col: int, direction: str):
                 work_queue.append((new_row, new_col, 'R'))
 
 
-def part_one(map, en_map):
+def part_one(map, initial=(0, 0, 'R')):
+    en_map = make_2d_array(len(map), len(map[0]))
     global work_queue
     previous_work = {}
-    work_queue.append((0, 0, 'R'))
+    work_queue.append(initial)
     while len(work_queue) > 0:
         row, col, direction = work_queue.pop(0)
         if (row, col, direction) in previous_work:
             continue
         previous_work[(row, col, direction)] = True
-        process_step(map, row, col, direction)
+        process_step(map, en_map, row, col, direction)
 
         # log.debug(f'After step: {row}, {col}, {direction} {map[row][col]} we have {work_queue=}')
     for row in en_map:
@@ -80,13 +79,31 @@ def part_one(map, en_map):
 
     en_count = sum([sum(row) for row in en_map])
     log.info(f"Part one: {en_count}")
+    return en_count
+
+
+def part_two(map):
+    max_score = 0
+    # Top row, going downwards
+    for col in range(len(map[0])):
+        max_score = max(max_score, part_one(map, (0, col, 'D')))
+    # Right column, going left
+    for row in range(len(map)):
+        max_score = max(max_score, part_one(map, (row, len(map[0]) - 1, 'L')))
+    # Bottom row, going upwards
+    for col in range(len(map[0])):
+        max_score = max(max_score, part_one(map, (len(map) - 1, col, 'U')))
+    # Left column, going right
+    for row in range(len(map)):
+        max_score = max(max_score, part_one(map, (row, 0, 'R')))
+
+    log.info(f"Part two: {max_score}")
 
 
 if __name__ == '__main__':
     log.setLevel('DEBUG')
     sample, full = load_2d_arrays(16)
     energized_map = make_2d_array(len(sample), len(sample[0]))
-    part_one(sample, energized_map)
-
-    energized_map = make_2d_array(len(full), len(full[0]))
-    part_one(full, energized_map)
+    part_one(sample)
+    part_one(full)
+    part_two(full)
