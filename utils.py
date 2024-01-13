9659f8f61cfe531e6b94735ee420672918d012bd
Paul Hubbard <pfh@phfactor.net>
2024-01-13 15:44:09 -0800

    Better/cleaner Dijkstra.

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
-
+from collections import defaultdict
+import heapq
 from itertools import permutations, combinations_with_replacement
 import logging
 
@@ -241,3 +242,38 @@ class Graph:
 #  END
 #
 ##########################################################
+# Found a better implementation that doesn't reinvent the priority queue.
+# See https://stackoverflow.com/questions/22897209/dijkstras-algorithm-in-python
+
+# graph = {
+#     'A': [('B', 2), ('C', 1)],
+#     'B': [('A', 2), ('C', 4), ('D', 3)],
+#     'C': [('A', 1), ('B', 4), ('E', 2)],
+#     'E': [('C', 2), ('D', 1), ('F', 4)],
+#     'D': [('B', 3), ('E', 1), ('F', 2)],
+#     'F': [('D', 2), ('E', 4)]
+#
+# }
+
+
+def dijkstra(graph, start: str):
+    result_map = defaultdict(lambda: float('inf'))
+    result_map[start] = 0
+
+    visited = set()
+
+    queue = [(0, start)]
+
+    while queue:
+        weight, v = heapq.heappop(queue)
+        visited.add(v)
+
+        for u, w in graph[v]:
+            if u not in visited:
+                result_map[u] = min(w + weight, result_map[u])
+                heapq.heappush(queue, [w + weight, u])
+
+    return result_map
+
+
+# print(dijkstra(graph, 'A'))
